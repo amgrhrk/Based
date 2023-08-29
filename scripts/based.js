105a66d76bae06d0ae48757fb22acb71fdcf78df
amgrhrk <67768560+amgrhrk@users.noreply.github.com>
2023-08-29 00:48:11 -0400

    Add auto compression

--- scripts/based.js
+++ scripts/based.js
@@ -1,7 +1,14 @@
 const Based = {
-    async encode(text) {
-        const blob = new Blob([text]);
-        const uint8Array = new Uint8Array(await blob.arrayBuffer());
+    alphabet: ['典', '孝', '急', '赢'],
+    async encode(text, compress = false) {
+        let uint8Array;
+        if (compress) {
+            uint8Array = LZString.compressToUint8Array(text);
+        }
+        else {
+            const blob = new Blob([text]);
+            uint8Array = new Uint8Array(await blob.arrayBuffer());
+        }
         return [...uint8Array]
             .map(byte => byte.toString(4).padStart(4, '0'))
             .flatMap(token => [...token].map(digit => {
@@ -10,11 +17,10 @@ const Based = {
                     case '1': return '孝';
                     case '2': return '急';
                     case '3': return '赢';
-                    default: return '?';
                 }
             })).join('');
     },
-    async decode(based) {
+    async decode(based, compress = false) {
         const uint8Data = [];
         for (let i = 0; i < based.length; i += 4) {
             const base4Number = [...based.slice(i, i + 4)].map(c => {
@@ -28,7 +34,11 @@ const Based = {
             }).join('');
             uint8Data.push(parseInt(base4Number, 4));
         }
-        const blob = new Blob([new Uint8Array(uint8Data)]);
+        const uint8Array = new Uint8Array(uint8Data);
+        if (compress) {
+            return LZString.decompressFromUint8Array(uint8Array);
+        }
+        const blob = new Blob([uint8Array]);
         return blob.text();
     }
 };
--- scripts/index.js
+++ scripts/index.js
@@ -6,53 +6,77 @@ const rightPanel = {
 	self: document.querySelectorAll('.panel')[1],
 	textarea: document.getElementById('encoded')
 }
-const defaultHeight = original.scrollHeight
-let isTyping = false
+const defaultHeight = leftPanel.textarea.scrollHeight
+const state = {
+	ENCODING: 0,
+	DECODING: 1,
+	current: 0,
+	typing: false
+}
 
 async function encode() {
-	rightPanel.textarea.value = await Based.encode(leftPanel.textarea.value)
+	state.current = state.ENCODING
+	try {
+		const resultCompressed = await Based.encode(leftPanel.textarea.value, true)
+		const resultUncompressed = await Based.encode(leftPanel.textarea.value, false)
+		if (resultCompressed.length < resultUncompressed.length - 1) {
+			rightPanel.textarea.value = resultCompressed + Based.alphabet[Math.floor(Math.random() * Based.alphabet.length)]
+		} else {
+			rightPanel.textarea.value = resultUncompressed
+		}
+	} catch (err) {
+		const resultUncompressed = await Based.encode(leftPanel.textarea.value, false)
+		rightPanel.textarea.value = resultUncompressed
+	}
 }
 async function decode() {
+	state.current = state.DECODING
 	try {
-		leftPanel.textarea.value = await Based.decode(rightPanel.textarea.value)
+		leftPanel.textarea.value = await Based.decode(rightPanel.textarea.value.trim(), leftPanel.textarea.value.length % 4 !== 0)
 	} catch (err) {}
 }
 function resize() {
 	leftPanel.textarea.style.height = defaultHeight + 'px'
 	rightPanel.textarea.style.height = defaultHeight + 'px'
-	const height = Math.max(leftPanel.textarea.scrollHeight, rightPanel.textarea.scrollHeight)
-	leftPanel.textarea.style.height = height + 'px'
-	rightPanel.textarea.style.height = height + 'px'
+	if (window.getComputedStyle(document.querySelector('.main')).flexDirection === 'row') {
+		const height = Math.max(leftPanel.textarea.scrollHeight, rightPanel.textarea.scrollHeight)
+		leftPanel.textarea.style.height = height + 'px'
+		rightPanel.textarea.style.height = height + 'px'
+	} else {
+		leftPanel.textarea.style.height = leftPanel.textarea.scrollHeight + 'px'
+		rightPanel.textarea.style.height = rightPanel.textarea.scrollHeight + 'px'
+	}
 }
 
 leftPanel.textarea.addEventListener('compositionstart', () => {
-	isTyping = true
+	state.typing = true
 })
 rightPanel.textarea.addEventListener('compositionstart', () => {
-	isTyping = true
+	state.typing = true
 })
 leftPanel.textarea.addEventListener('compositionend', async () => {
-	isTyping = false
+	state.typing = false
 	await encode()
 	resize()
 })
 rightPanel.textarea.addEventListener('compositionend', async () => {
-	isTyping = false
+	state.typing = false
 	await decode()
 	resize()
 })
 
 leftPanel.textarea.addEventListener('input', async () => {
-	if (isTyping) {
+	if (state.typing) {
 		return
 	}
 	await encode()
 	resize()
 })
 rightPanel.textarea.addEventListener('input', async () => {
-	if (isTyping) {
+	if (state.typing) {
 		return
 	}
 	await decode()
 	resize()
 })
+window.addEventListener('resize', resize)
